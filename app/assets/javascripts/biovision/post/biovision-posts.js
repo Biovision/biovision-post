f7687342802bd72448819c37d22941e3381bdc85
Maxim Khan-Magomedov <maxim.km@gmail.com>
2018-06-17 18:06:49 +0300

    Dynamically loading more posts

---
+++ app/assets/javascripts/biovision/post/biovision-posts.js
@@ -0,0 +1,50 @@
+'use strict';
+
+const BiovisionPosts = {
+    loadPosts: function (e) {
+        e.preventDefault();
+
+        const button = this;
+        const container = document.getElementById(button.getAttribute('data-container'));
+        if (container) {
+            const list = container.querySelector('div.list');
+
+            if (list && !button.classList.contains('loading')) {
+                const url = button.getAttribute('href');
+                const request = Biovision.new_ajax_request('get', url, function () {
+                    button.classList.remove('loading');
+
+                    const response = JSON.parse(this.responseText);
+                    let nextLink;
+                    if (response.hasOwnProperty('data')) {
+                        response.data.forEach(function (element) {
+                            if (element.hasOwnProperty('meta')) {
+                                list.insertAdjacentHTML('beforeEnd', element.meta['html_preview']);
+                            }
+                        });
+                    }
+                    if (response.hasOwnProperty('links')) {
+                        const links = response.links;
+                        if (links.hasOwnProperty('next')) {
+                            nextLink = links.next;
+                        }
+                    }
+                    if (nextLink) {
+                        button.setAttribute('href', nextLink);
+                    } else {
+                        button.classList.add('hide');
+                    }
+                });
+
+                button.classList.add('loading');
+                request.send();
+            }
+        }
+    }
+};
+
+document.addEventListener('DOMContentLoaded', function () {
+    document.querySelectorAll('.posts-loader').forEach(function (button) {
+        button.addEventListener('click', BiovisionPosts.loadPosts);
+    });
+});
--- lib/biovision/post/version.rb
+++ lib/biovision/post/version.rb
@@ -1,5 +1,5 @@
 module Biovision
   module Post
-    VERSION = '0.5.180616'
+    VERSION = '0.5.180617'
   end
 end
