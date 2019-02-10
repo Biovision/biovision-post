c2c0541cf9edb9ef343d8be1078dd4f53a2e3b0b
Maxim Khan-Magomedov <maxim.km@gmail.com>
2019-02-10 20:43:41 +0300

    Code style for post body parsing; aside tags

--- app/services/post_parser.rb
+++ app/services/post_parser.rb
@@ -1,62 +1,76 @@
+# frozen_string_literal: true
+
+# Parser for post body
 class PostParser
+  # @param [Post] entity
   def initialize(entity)
     @entity = entity
     @body   = entity.body.clone
   end
 
   def parsed_body
-    escape_scripts
-    convert_video_links
-    @body
+    @body = escape_scripts
+    @body = convert_video_links
+    @body = convert_asides
   end
 
   def escape_scripts
-    @body.gsub!(/<script/, '&lt;script')
+    @body.gsub(/<script/, '&lt;script')
   end
 
   def convert_video_links
-    pattern = /<p>{video:(?<url>[^}]{1,100})}<\/p>/i
-    @body.gsub! pattern do |chunk|
-      iframe = video_url_to_iframe(pattern.match(chunk)[:url])
-      "<div class=\"proportional-container r-16x9\">#{iframe}</div>"
+    pattern = %r(<p>{video:(?<url>[^}]{1,100})}</p>)i
+    @body.gsub(pattern) do
+      iframe = video_url_to_iframe($LAST_MATCH_INFO[:url])
+      %(<div class="proportional-container r-16x9">#{iframe}</div>)
+    end
+  end
+
+  def convert_asides
+    pattern = %r{<p>~~\s*(.+?)</p>}i
+    @body.gsub(pattern) do
+      "<aside>#{$LAST_MATCH_INFO[1]}</aside>"
     end
   end
 
   private
 
+  # @param [String] url
   def video_url_to_iframe(url)
-    if url =~ /\Ahttps?:\/\/vimeo\.com/
-      src = process_vimeo_link(url)
-    elsif url =~ /\Ahttps?:\/\/www\.youtube\.com\/watch/
-      src = process_youtube_link(url)
-    elsif url =~ /\Ahttps?:\/\/youtu\.be\/.+/
-      src = process_youtu_be_link(url)
-    else
-      src = ''
-    end
+    src = video_iframe_url(url)
 
-    if src.blank?
-      url
-    else
-      "<iframe src=\"#{src}\" allowfullscreen></iframe>"
+    src.blank? ? url : %(<iframe src="#{src}" allowfullscreen></iframe>)
+  end
+
+  # @param [String] url
+  def video_iframe_url(url)
+    if url.match?(%r{\Ahttps?://vimeo\.com})
+      process_vimeo_link(url)
+    elsif url.match?(%r{\Ahttps?://www\.youtube\.com/watch})
+      process_youtube_link(url)
+    elsif url.match?(%r{\Ahttps?://youtu\.be/.+})
+      process_youtu_be_link(url)
     end
   end
 
+  # @param [String] url
   def process_vimeo_link(url)
-    video_id  = url.gsub(/\A\D+\/(\d+)\/?/, "\\1")
+    video_id  = url.gsub(%r{\A\D+/(\d+)/?}, '\\1')
     api_url   = "https://vimeo.com/api/v2/video/#{video_id}.json"
-    json_data = JSON.load(open(api_url)).first
+    json_data = JSON.parse(URI.open(api_url)).first
 
     json_data.blank? ? '' : "https://player.vimeo.com/video/#{json_data['id']}"
   end
 
+  # @param [String] url
   def process_youtube_link(url)
     video_id = CGI.parse(URI.parse(url).query)['v'].first
     "//www.youtube.com/embed/#{video_id}"
   end
 
+  # @param [String] url
   def process_youtu_be_link(url)
-    video_id = URI.parse(url).path.gsub('/', '')
+    video_id = URI.parse(url).path.delete('/')
     "//www.youtube.com/embed/#{video_id}"
   end
 end
